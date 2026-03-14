dc4b049bb59082becb926128b9e927d313f68112
Akanksha Chaurasia <168443992+akankshachaurasia14@users.noreply.github.com>
2026-03-14 05:49:38 +0000

    datatype summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+//Primitve datatype
+
+// 7 types : string , Number , Boolean, null,undefined , Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+//Reference (non primitive)
+
+//Array,Objects, functions
+
+const heros = ["shaktiman","Captain America","Ironman"];
+let myObj = { name: "Akanksha",age: 22,}
+
+const myFunnction = function(){
+    console.log("Hello world");
+
+    console.log(typeof anotherId);
+    
+    
+}
